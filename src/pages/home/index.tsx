af029a32065f21ae6bef2d7ef7ca85eb35e16ca6
Danilo Katayama <dak@vortx.com.br>
2023-10-24 17:40:57 -0300

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Container, Hero, Preview } from './styles'
 
 import previewImage from '../../assets/app-preview.png'
-import { ClaimUsernameForm } from '../components/ClaimUsernameForm'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
